bfc0443548559b4c402c532410f1916d82139c2a
littlefox271 <96527727+littlefox271@users.noreply.github.com>
2023-03-09 00:28:37 +0700

    Update 2_programming_technique.cpp

--- 2_programming_technique.cpp
+++ 2_programming_technique.cpp
@@ -2,5 +2,44 @@
 using namespace std;       // declares that the classes and functions of standard library can be used directly in the code
 
 int main () {
-// solution comes here
+    ios::sync_with_stdio(0); // make input and output more efficient
+    cin.tie(0);
+
+    // Note that "\n" works faster than endl, because endl always causes a flush operation
+
+    string s;
+    getline(cin, s) // read a whole input line, containing space
+
+    while (cin >> x) {
+        // if the data is unknown
+    }
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout); // when files are used for input output
+
+    // Modular Arithmetric
+    // (a+b) mod m = (a mod m + b mod m) mod
+    // (a-b) mod m = (a mod m - b mod m) mod
+    // (a.b) mod m = (a mod m . b mod m) mod
+    long long x = 1;
+    for (int i = 1; i <= n; i++) {
+        x = (x*i) % m;
+    }
+    cout << x << "\n";  // caculate n!
+
+    // to make sure there are no negative remainders
+    x = x % m;
+    if (x < 0) x += m;
+
+
+    // shorten code
+    typedef long long ll;
+    typedef vector<int> vi;
+    typedef pair<int, int> pi;
+    #define F first;
+    #define S second;
+    #define PB push_back;
+    #define MP make_pair;
+    #define REP(i,a,b) for (int i = a; i <= b; i++);
+
 }
