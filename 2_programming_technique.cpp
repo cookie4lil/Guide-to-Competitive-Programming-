cc52d8a8feb120f2db994605ac6931af4d6b941f
littlefox271 <96527727+littlefox271@users.noreply.github.com>
2023-03-08 23:08:17 +0700

    Create 2_programming_technique.cpp

---
+++ 2_programming_technique.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>   // a feature of g++ compiler allows to include the entire library
+using namespace std;       // declares that the classes and functions of standard library can be used directly in the code
+
+int main () {
+// solution comes here
+}
